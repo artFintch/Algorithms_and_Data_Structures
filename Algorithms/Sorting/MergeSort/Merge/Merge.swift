73555a509ba863deb7bb157ac8eb807b72fd5cfe
Vyacheslav Khorkov <artfintch@ya.ru>
2017-08-20 12:23:39 +0500

    Change merge function.

--- Algorithms/Sorting/MergeSort/Merge/Merge.swift
+++ Algorithms/Sorting/MergeSort/Merge/Merge.swift
@@ -1,16 +1,31 @@
 //
 //  Merge.swift
-//  Algorithms and Data Structures
+//  Merge two arrays
 //
-//  Created by Vyacheslav Khorkov on 03/08/2017.
+//  Created by Vyacheslav Khorkov on 20/08/2017.
 //  Copyright © 2017 Vyacheslav Khorkov. All rights reserved.
 //
 
 import Foundation
 
-func merge(_ a: inout [String], _ b: inout [String], _ r: inout [String]) {
-    if a.isEmpty { return r.append(contentsOf: b) }
-    if b.isEmpty { return r.append(contentsOf: a) }
-    r.append(a[0] < b[0] ? a.removeFirst() : b.removeFirst())
-    merge(&a, &b, &r)
+// Time: O(n + m)
+// Memory: O(n + m)
+func merge<T: Comparable>(_ a: [T], _ b: [T]) -> [T] {
+	var i = 0, j = 0
+	var r: [T] = []
+	
+	while i < a.count && j < b.count {
+		if a[i] < b[j] {
+			r.append(a[i])
+			i += 1
+		} else {
+			r.append(b[j])
+			j += 1
+		}
+	}
+	
+	(i..<a.count).forEach { r.append(a[$0]) }
+	(j..<b.count).forEach { r.append(b[$0]) }
+	
+	return r
 }
